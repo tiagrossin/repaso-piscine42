bb376876b701eb86cd64fef74862eb8093cfc350
tiagrossin <tiagoyjose@gmail.com>
2024-07-29 21:15:25 +0200

    added ex03

--- c00/ex03/ft_print_numbers.c
+++ c00/ex03/ft_print_numbers.c
@@ -10,4 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+void	ft_print_numbers(void)
+{
+	int	i;
+
+	i = '0';
+	while (i <= '9')
+	{
+		write(1, &i, 1);
+		i++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_numbers();
+	return (0);
+}*/
