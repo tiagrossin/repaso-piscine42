fa79211b9e2fec1b3dbbc26112a6a09a6bd9c5ee
tiagrossin <tiagoyjose@gmail.com>
2024-07-30 21:11:05 +0200

    added ex05

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -10,4 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+void	ft_putnum(int n)
+{
+	n = n + '0';
+	write(1, &n, 1);
+}
+
+void	ft_print_comb(void)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				ft_putnum(i);
+				ft_putnum(j);
+				ft_putnum(k);
+				if (i != 7)
+					write(1, ", ", 2);
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	write(1, "\n", 1);
+}
+/*int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}*/
