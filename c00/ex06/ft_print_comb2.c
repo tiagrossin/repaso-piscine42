a57782a212cde2c65047743e43bb3a28d6ac2529
tiagrossin <tiagoyjose@gmail.com>
2024-08-01 12:37:18 +0200

    finished ex06, started working on ex07 and reorganized folder files

--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: tiagrossin <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 21:12:08 by tiagrossi         #+#    #+#             */
-/*   Updated: 2024/07/30 21:12:15 by tiagrossi        ###   ########.fr       */
+/*   Updated: 2024/07/31 12:37:34 by tiagrossi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,50 @@
 
 void	ft_putnum(int n)
 {
-	n /= 10;
-	n += '0';
-	write(1, &n, 1);
+	int	temp;
+
+	temp = n;
+	if (n > 9)
+	{
+		n /= 10;
+		n += '0';
+		write (1, &n, 1);
+	}
+	else
+	{
+		write (1, "0", 1);
+	}
+	n = temp;
 	n %= 10;
 	n += '0';
-	write(1, &n, 1);
+	write (1, &n, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_putnum(i);
+			write(1, " ", 1);
+			ft_putnum(j);
+			if (i != 98)
+					 write(1, ", ", 2);
+			j++;
+		}
+		i++;
+	}
+	write(1, "\n", 1);
 }
 
 int	main(void)
 {
-	ft_putnum(42);
+	ft_print_comb2();
 	return (0);
 }
