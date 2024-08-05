56711392c2991420f5655b802c4d1c325f2f4a8b
tiagrossin <tiagoyjose@gmail.com>
2024-08-05 14:03:38 +0200

    modifications to ex05 & ex06 to test mini moulinette

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiagrossin <marvin@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 19:55:00 by tiagrossi         #+#    #+#             */
+/*   Updated: 2024/07/30 19:55:17 by tiagrossi        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnum(int n)
+{
+	n = n + '0';
+	write(1, &n, 1);
+}
+
+void	ft_print_comb(void)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				ft_putnum(i);
+				ft_putnum(j);
+				ft_putnum(k);
+				if (i != 7)
+					write(1, ", ", 2);
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+/*int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}*/
---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiagrossin <marvin@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 21:12:08 by tiagrossi         #+#    #+#             */
+/*   Updated: 2024/08/01 12:44:59 by tiagrossi        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnum(int n)
+{
+	int	temp;
+
+	temp = n;
+	if (n > 9)
+	{
+		n /= 10;
+		n += '0';
+		write (1, &n, 1);
+	}
+	else
+	{
+		write (1, "0", 1);
+	}
+	n = temp;
+	n %= 10;
+	n += '0';
+	write (1, &n, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_putnum(i);
+			write(1, " ", 1);
+			ft_putnum(j);
+			if (i != 98)
+					 write(1, ", ", 2);
+			j++;
+		}
+		i++;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_comb2();
+	return (0);
+}*/
