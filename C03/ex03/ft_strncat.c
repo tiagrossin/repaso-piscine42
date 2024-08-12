ec6930e8f3fb84c8cb640ea1b50a2de2ff210025
Santiago <tiagoyjose@gmail.com>
2024-08-12 21:02:05 +0200

    added complete ex03

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiagrossin <marvin@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 20:55:46 by tiagrossi         #+#    #+#             */
+/*   Updated: 2024/08/12 20:55:49 by tiagrossi        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	char	*ptr;
+	unsigned int i;
+
+	ptr = dest;
+	i = 0;
+	while (*ptr)
+		ptr++;
+	while (*src && i < nb)
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+		i++;
+	}
+	*ptr = '\0';
+	return (dest);
+}
