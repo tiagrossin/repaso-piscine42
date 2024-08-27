ba05fd5c30c8fe0b49b3db0df7d8916a4c008203
Santiago <tiagoyjose@gmail.com>
2024-08-27 10:43:31 +0200

    created C08 folder and added complete ex00

---
+++ C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiagrossin <marvin@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/27 10:24:42 by tiagrossi         #+#    #+#             */
+/*   Updated: 2024/08/27 10:24:49 by tiagrossi        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int	*a, int	*b);
+void	ft_putstr(char	*str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
